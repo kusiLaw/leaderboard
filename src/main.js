9f70a6202b0f658aca10d89d1f223ebabb24a602
kusi <lawcubegsm@gmail.com>
2022-09-01 09:30:16 +0000

    Fix linter error

--- src/main.js
+++ src/main.js
@@ -1,52 +1,43 @@
 import generateList from './module/generate_list.js';
 import Api from './module/api.js';
 
-
 const form = document.getElementById('add-form');
 const error = document.getElementById('error');
-let api = new Api()
+const api = new Api();
 
-
-const  loadList = async() =>{
- try{
-  let data = await api.getScores()
-  const container = document.getElementById('scoreListContainer');
-  container.innerHTML = '';
-  container.appendChild(generateList(data));
- }catch{
-  error.innerHTML = "Server not responding" 
- }
- 
-}
-
-window.onload = () => {
- loadList()
+const loadList = async () => {
+  try {
+    const data = await api.getScores();
+    const container = document.getElementById('scoreListContainer');
+    container.innerHTML = '';
+    container.appendChild(generateList(data));
+  } catch {
+    error.innerHTML = 'Server not responding';
+  }
 };
 
+window.onload = () => {
+  loadList();
+};
 
-document.getElementById('refresh').onclick = () =>{
- loadList()
-}
+document.getElementById('refresh').onclick = () => {
+  loadList();
+};
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const playerName = form.elements['p-name'];
   const playerScores = form.elements.scores;
-  if(playerName.value !== '' && playerScores.value !== ''){
-   api.newScore({ 
-    "user": playerName.value,
-    "score": playerScores.value
-   })
-   playerName.value =''
-   playerScores.value =''
-   loadList()
-  error.innerHTML = ''
-  }else{
-   error.innerHTML = 'Name or score should not be empty'
+  if (playerName.value !== '' && playerScores.value !== '') {
+    api.newScore({
+      user: playerName.value,
+      score: playerScores.value,
+    });
+    playerName.value = '';
+    playerScores.value = '';
+    loadList();
+    error.innerHTML = '';
+  } else {
+    error.innerHTML = 'Name or score should not be empty';
   }
 });
-
-
-
-
-
--- src/module/generate_list.js
+++ src/module/generate_list.js
@@ -1,16 +1,13 @@
 const generateList = (array) => {
-
- array = array.result
+  array = array.result;
 
   const ul = document.createElement('ul');
   ul.setAttribute('class', 'score-list');
 
-  for(let i=0; i<array.length; i += 1){
- 
+  for (let i = 0; i < array.length; i += 1) {
     ul.innerHTML += `<li class="list-items">
    <p><span>${array[i].user}:</span><span class="score-val">${array[i].score}</span></p>
    </li>`;
-
   }
   return ul;
 };
